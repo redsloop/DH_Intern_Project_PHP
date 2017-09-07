88fae95ee1f4f647399465b643b78ec7f24db35b
akasaka <redsloop.ko@gamil.com>
2017-09-07 16:04:13 +0900

    ファイル名を変更しました。

--- functionAndVarForFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-$filename = 'toDoList.txt';
-
-function writeDateInFile ($date, $filename) {
-    unlink($filename);
-    file_put_contents($filename, json_encode($date).PHP_EOL, FILE_APPEND);
-
-}
